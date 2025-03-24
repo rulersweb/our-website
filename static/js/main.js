f33463bf3449329d504d33995ad1723947d75512
Aakash Chokshi <rulersweb@gmail.com>
2025-03-24 18:38:01 +0530

    [UPD] Mobile responsive changes

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,18 @@
 function toggleTheme() {
     const body = document.body;
     const themeToggleBtn = document.getElementById('themeToggle');
+    const themeToggleMobileBtn = document.getElementById('themeToggleMobile');
     const isLightTheme = body.classList.contains('light-theme');
     
     if (isLightTheme) {
         body.classList.remove('light-theme');
         themeToggleBtn.innerHTML = '<i class="fas fa-sun"></i> Light Mode';
+        themeToggleMobileBtn.innerHTML = '<i class="fas fa-sun"></i>';
         localStorage.setItem('theme', 'dark');
     } else {
         body.classList.add('light-theme');
         themeToggleBtn.innerHTML = '<i class="fas fa-moon"></i> Dark Mode';
+        themeToggleMobileBtn.innerHTML = '<i class="fas fa-moon"></i>';
         localStorage.setItem('theme', 'light');
     }
 }
@@ -36,8 +39,10 @@ document.addEventListener('DOMContentLoaded', function() {
     if (savedTheme === 'dark') {
         document.body.classList.remove('light-theme');
         document.getElementById('themeToggle').innerHTML = '<i class="fas fa-sun"></i> Light Mode';
+        document.getElementById('themeToggleMobile').innerHTML = '<i class="fas fa-sun"></i>';
     } else {
         document.body.classList.add('light-theme');
         document.getElementById('themeToggle').innerHTML = '<i class="fas fa-moon"></i> Dark Mode';
+        document.getElementById('themeToggleMobile').innerHTML = '<i class="fas fa-moon"></i>';
     }
 });
